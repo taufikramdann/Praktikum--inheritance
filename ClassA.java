5ba050e372391d35316dece4fe1cdc2310d60b2d
taufikramdann <115464057+taufikramdann@users.noreply.github.com>
2022-10-27 12:13:11 +0700

    Create ClassA.java

---
+++ ClassA.java
@@ -0,0 +1,10 @@
+public class ClassA {
+    String var_a = "Variabel A";
+    String var_b = "Variabel B";
+    String var_c = "Variabel C";
+    String var_d = "Variabel D";
+    
+    ClassA(){
+        System.out.println("Konstruktor A dijalankan");
+ }
+}
