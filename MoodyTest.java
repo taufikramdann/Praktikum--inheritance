564587f88bfdd163f45c58081ca2e7f6b8e5bacf
taufikramdann <115464057+taufikramdann@users.noreply.github.com>
2022-10-27 13:43:40 +0700

    Create MoodyTest.java

---
+++ MoodyTest.java
@@ -0,0 +1,11 @@
+ MoodyTest kelas  publik {
+    public  static  void  main ( String [] args ) {
+        MoodyObject  m = MoodyObject baru  ();
+        SadObject  Sedih = SadObject baru  ();
+        HappyObject  Happy = HappyObject baru  ();
+        
+        m . berbicara ();
+        Sedih . menangis ();
+        Senang . tertawa ();
+    }
+}
