818000acdd71e29c604d8236dec0b82fe6732bb1
taufikramdann <115464057+taufikramdann@users.noreply.github.com>
2022-10-27 11:23:55 +0700

    Create Baby.java

---
+++ Baby.java
@@ -0,0 +1,16 @@
+kelas  publik Bayi  memperluas  Induk {
+    String  nama bayi ;
+
+    publik  String  getBabyName () {
+        kembali  babyName ;
+    }
+    Bayi ( String  nama bayi ){
+        super ();
+        ini . namabayi = namabayi ;
+        Sistem . keluar . println ( "Konstruktor Bayi" );
+        Sistem . keluar . println ( namabayi );
+}
+     tangisan kekosongan  publik () {
+        Sistem . keluar . println ( "owek hutang" );
+    }
+}
