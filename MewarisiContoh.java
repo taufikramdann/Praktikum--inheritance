d82f1a24a522c187cd31b4d864f6dddbd5bc2917
taufikramdann <115464057+taufikramdann@users.noreply.github.com>
2022-10-27 13:36:16 +0700

    Create MewarisiContoh.java

---
+++ MewarisiContoh.java
@@ -0,0 +1,18 @@
+ kelas  publik InheritContoh {
+    
+public  static  void  main ( String [] args ) {
+    Bapak  objekBapak = Bapak baru  ();
+    Anak  objectAnak = Anak baru  ();
+    
+
+
+objekBapa . a = 1 ;
+objekBapa . b = 1 ;
+Sistem . keluar . println ( "Objek Bapak (Kelas Super):" );
+objekBapa . tampilkan_variabel ();
+
+objekAnak . c = 5 ;
+Sistem . keluar . println ( "Objek Anak (Superclass dari Bapak):" );
+objekAnak . show_Variabel ();
+}
+}
