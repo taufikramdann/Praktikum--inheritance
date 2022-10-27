131bdfb4c4ba581fb3ff0281a16d129bdf0b7578
taufikramdann <115464057+taufikramdann@users.noreply.github.com>
2022-10-27 13:39:50 +0700

    Create MoodyObject.java

---
+++ MoodyObject.java
@@ -0,0 +1,8 @@
+ kelas  publik MoodyObject {
+    dilindungi  String  getMood (){
+        kembali  "murung" ;
+    }
+    public  void  berbicara (){
+        Sistem . keluar . println ( "Saya : " + getMood ());
+    }
+}
