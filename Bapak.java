72a3f8a45f92570ca259103ca1bdcbca2c517fac
taufikramdann <115464057+taufikramdann@users.noreply.github.com>
2022-10-27 11:27:05 +0700

    Create Bapak.java

---
+++ Bapak.java
@@ -0,0 +1,9 @@
+ kelas  publik Bapak {
+    dalam  sebuah ;
+    int  b ;
+    
+public  void  show_variabel (){
+Sistem . keluar . println ( "NIlai a=" + a );
+Sistem . keluar . println ( "NIlai b=" + b );
+}
+}
