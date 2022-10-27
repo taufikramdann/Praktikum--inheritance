af8a53a489de5b2f0c0121d505b0bd272613fc05
taufikramdann <115464057+taufikramdann@users.noreply.github.com>
2022-10-27 13:47:38 +0700

    Create Induk.java

---
+++ Induk.java
@@ -0,0 +1,12 @@
+ kelas  publik Induk {
+    String  nama induk ;
+     Induk publik () {}
+
+    publik  String  getParentName () {
+        kembali  parentName ;
+    } Induk publik  ( String parentName ){ 
+        ini . parentName = parentName ;
+        Sistem . keluar . println ( "Induk Konstruktor" );
+}
+
+    }
