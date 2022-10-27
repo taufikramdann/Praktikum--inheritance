e9d3cf8794203b15b8006f7d659ea533bc600c59
taufikramdann <115464057+taufikramdann@users.noreply.github.com>
2022-10-27 14:05:32 +0700

    Create SadObject.java

---
+++ SadObject.java
@@ -0,0 +1,9 @@
+ kelas  publik SadObject  memperluas  MoodyObject {
+   
+    Protected  String  getSad (){
+        kembali  "sedih" ;
+    }
+     tangisan kosong  publik (){
+        Sistem . keluar . println ( "Aduh : " + getSad ());
+}
+}
