4a79cd9cbdd6e27bc5f3168dec200cc8bf4c84bc
taufikramdann <115464057+taufikramdann@users.noreply.github.com>
2022-10-27 13:49:23 +0700

    Create Pegawai.java

---
+++ Pegawai.java
@@ -0,0 +1,13 @@
+pegawai  kelas  umum {
+     nama String  publik ;
+     gaji ganda  publik ;
+}
+
+Kelas  Manajer  Perpanjangan  Pegawai {
+     departemen String  publik ;
+    
+    public  void  IsiData ( String  n , String  d ){
+        nama = n ;
+        jurusan = d ;
+    }
+}
