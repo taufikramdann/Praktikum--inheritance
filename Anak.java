78e0c27a7d1dcba253212bfe899ae34dccd3120a
taufikramdann <115464057+taufikramdann@users.noreply.github.com>
2022-10-27 11:21:40 +0700

    Create Anak.java

---
+++ Anak.java
@@ -0,0 +1,8 @@
+public  class  Anak  extends  Bapak {
+    int  c ;
+public  void  show_Variabel (){
+    Sistem . keluar . println ( "NIlai a=" + super .a ) ;
+    Sistem . keluar . println ( "NIlai b=" + super .b ) ;
+    Sistem . keluar . println ( "NIlai c=" + c );
+}
+}
